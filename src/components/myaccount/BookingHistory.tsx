e55cfdd09bd283f9dca7d580ae400314ba4e246e
kavisheak <skavisheak.official@gmail.com>
2026-09-17 15:40:02 +0530

    fixed typescript errors 3

--- src/components/myaccount/BookingHistory.tsx
+++ src/components/myaccount/BookingHistory.tsx
@@ -270,7 +270,7 @@ export default function BookingHistory() {
         filename: `Invoice_${refName.toUpperCase()}.pdf`,
         image: { type: 'jpeg' as const, quality: 0.98 },
         html2canvas: { scale: 2, useCORS: true },
-        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        jsPDF: { unit: 'mm' as const, format: 'a4' as const, orientation: 'portrait' as const }
       };
       
       html2pdf().set(opt).from(container).save();
